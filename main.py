51b993154b3860635e580b03d3cb9b381087f560
faroemiliano <emilianofaro@MacBook-Pro-de-Emiliano.local>
2026-04-04 19:43:17 -0300

    estilos personalizados desde la db reset false

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app = FastAPI(title="Barbería API")
 # =====================
 # RESET SOLO EN DESARROLLO
 # =====================
-RESET_DB = True
+RESET_DB = False
   # ⚠️ poner False en producción
 
 if RESET_DB:
